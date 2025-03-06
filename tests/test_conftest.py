9cc0b282da172b5ea7e1fd9d6fdf9e55dc809e5a
Bhoomi Darji <bhoomi.pysquad@gmail.com>
2025-03-06 10:29:46 +0530

    6/3

---
+++ tests/test_conftest.py
@@ -0,0 +1,18 @@
+# to test that url successfully work with given username and password
+def test_conf_test1(setup):
+    driver = setup
+
+    act_url = driver.current_url
+    if act_url == "https://www.saucedemo.com/inventory.html":
+        assert True
+        driver.close()
+    else:
+        driver.save_screenshot("openurl.png")
+        assert False
+
+
+#to test that Url successfully opened
+def test_conf_test2(setup1):
+    driver = setup1
+
+    assert driver.current_url == "https://www.saucedemo.com/"
